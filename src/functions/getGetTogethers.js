29436eb6fa492e1d1f8f0fb767b18125cef94519
aitor <aitor@runroom.com>
2019-10-25 17:34:44 +0200

    all lessons finished

--- src/functions/getGetTogethers.js
+++ src/functions/getGetTogethers.js
@@ -1,12 +1,28 @@
 const AWSXray = require("aws-xray-sdk");
 AWSXray.captureAWS(require("aws-sdk"));
 
+const http = require("http");
 const AWS = require("aws-sdk");
 const middy = require('middy')
 const { ssm } = require('middy/middlewares')
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
+const FunctionShield = require('@puresec/function-shield');
+
+FunctionShield.configure({
+    policy: {
+        // 'block' mode => active blocking
+        // 'alert' mode => log only
+        // 'allow' mode => allowed, implicitly occurs if key does not exist
+        outbound_connectivity: "alert",
+        read_write_tmp: "block", 
+        create_child_process: "block",
+        read_handler: "block" },
+    token: process.env.functionShieldToken });
+
 const handler = async (event, context) => {
+
+    http.get('http://vgaltes.com');
 
     const req = {
         TableName: context.tableName,
@@ -16,7 +32,11 @@ const handler = async (event, context) => {
     const resp = await dynamodb.scan(req).promise();
     const res = {
         statusCode: 200,
-        body: JSON.stringify(resp.Items)
+        body: JSON.stringify(resp.Items),
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Credentials": true
+        }
     };
 
     return res;
